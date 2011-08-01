9302fafdd65d794dbfa294b6d830211606cf5cbd
sriram <sriram.in20@gmail.com>
2011-08-01 11:46:22 +0530

    aug112

--- app/models/tiny_print.rb
+++ app/models/tiny_print.rb
@@ -8,7 +8,7 @@ class TinyPrint < ActiveRecord::Base
 	               :detail_preview => [ "450x338", :jpg ] },
     #:path => "/prints/:id/:style.:extension",
     #:default_url => "/images/missing/prints/:style.png"
-:storage => :s3,
+      :storage => :s3,
       :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
       :bucket => "sharmanyros",
       :path => "/sriram/files/:style/:id/:filename" ,
